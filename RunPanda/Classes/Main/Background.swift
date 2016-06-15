e0756a3f6581b5cd266c3af89418aa4d47eca4d0
Rocky Ao <542622608@qq.com>
2016-06-15 18:48:29 +0800

    生成远景，近景属性

--- RunPanda/Classes/Main/Background.swift
+++ RunPanda/Classes/Main/Background.swift
@@ -10,5 +10,21 @@ import SpriteKit
 
 class Background: SKNode {
 
+    //近处背景数组
+    var backgroundArray = [SKSpriteNode]()
     
+    //远处背景数组
+    var farBackgroundArray = [SKSpriteNode]()
+    
+    
+    override init() {
+        super.init()
+        
+        
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
