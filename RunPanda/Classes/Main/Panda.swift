8bc938c695f5503ce8a6212936465839dfb44d8a
Rocky Ao <542622608@qq.com>
2016-06-15 16:13:17 +0800

    加入熊猫类

---
+++ RunPanda/Classes/Main/Panda.swift
@@ -0,0 +1,18 @@
+//
+//  Panda.swift
+//  RunPanda
+//
+//  Created by ZCBL on 16/6/15.
+//  Copyright © 2016年 RockyAo. All rights reserved.
+//
+
+import UIKit
+
+import SpriteKit
+
+
+class Panda: SKSpriteNode {
+    
+     lazy let runAtlas = SKTextureAtlas(named: "run.atlas")
+    
+}
